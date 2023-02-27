a2c2958e069cdc0920293261a65251fa63450a41
杨顾 <yanggu@enn.cn>
2023-02-27 21:32:17 +0800

    合并请求简单编写

--- metric-calculate-web/src/main/java/com/yanggu/metric_calculate/controller/TestController.java
+++ metric-calculate-web/src/main/java/com/yanggu/metric_calculate/controller/TestController.java
@@ -9,13 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Api(tags = "测试接口")
@@ -23,14 +20,15 @@ import java.util.stream.Collectors;
 @RequestMapping("/test")
 public class TestController {
 
-    private Queue<Request<String>> queue = new LinkedBlockingQueue<>(20);
+    private final Integer length = 20;
 
-    private ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(10);
+    private Integer intervalTime = 10;
 
-    @PostConstruct
-    public void init() {
-        //queue
-    }
+    private ScheduledFuture<?> scheduledFuture;
+
+    private final Queue<Request<String>> queue = new LinkedBlockingQueue<>(length);
+
+    private final ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(10);
 
     /**
      * 测试合并接口请求
@@ -39,32 +37,56 @@ public class TestController {
      */
     @GetMapping("/test1")
     public DeferredResult<String> test1() {
-        log.info(" 当前线程 外部 " + Thread.currentThread().getName());
-        DeferredResult<String> result = new DeferredResult<>(TimeUnit.SECONDS.toMillis(60L));
-        result.onTimeout(() -> log.warn("请求超时"));
-        result.onCompletion(() -> log.info("请求处理完成"));
+        log.info("当前线程外部 " + Thread.currentThread().getName());
+
+        DeferredResult<String> deferredResult = new DeferredResult<>(TimeUnit.SECONDS.toMillis(60L));
+
+        deferredResult.onTimeout(() -> log.warn("请求超时"));
+        deferredResult.onCompletion(() -> log.info("请求处理完成"));
         String uuid = IdUtil.fastSimpleUUID();
+
         log.info("uuid: {}", uuid);
+
         Request<String> request = new Request<>();
         request.setUuid(uuid);
-        request.setCompletableFuture(new CompletableFuture<>());
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+        request.setCompletableFuture(completableFuture);
 
+        addRequest(request);
 
-
-        return result;
+        completableFuture.whenCompleteAsync((result, throwable) -> {
+            deferredResult.setResult(result);
+        });
+        return deferredResult;
     }
 
     private void addRequest(Request<String> request) {
-        boolean offer = queue.offer(request);
-        if (!offer) {
-            //queue.poll()
-            List<Request<String>> collect = new ArrayList<>(queue);
-            for (Request<String> stringRequest : collect) {
-                log.info("uuid: {}, 数据被消费了", stringRequest.getUuid());
-                //设置完成的标志
-                stringRequest.getCompletableFuture().complete(request.getUuid());
-            }
+        queue.offer(request);
+        if (queue.size() >= length) {
+            flush();
+        }
+        //如果没有注册定时器, 注册一个定时器
+        if (scheduledFuture == null) {
+            this.scheduledFuture = scheduledExecutorService.schedule(this::flush, intervalTime, TimeUnit.MILLISECONDS);
+        }
+    }
 
+    private void flush() {
+        Request<String> request;
+        List<Request<String>> requestList = new ArrayList<>();
+        log.info("进入了flush方法");
+        while ((request = queue.poll()) != null) {
+            requestList.add(request);
+            log.info("执行poll方法");
+        }
+        for (Request<String> stringRequest : requestList) {
+            log.info("uuid: {}, 数据被消费了", stringRequest.getUuid());
+            //设置完成的标志
+            stringRequest.getCompletableFuture().complete(stringRequest.getUuid());
+        }
+        if (scheduledFuture != null) {
+            this.scheduledFuture.cancel(true);
+            this.scheduledFuture = null;
         }
     }
 
