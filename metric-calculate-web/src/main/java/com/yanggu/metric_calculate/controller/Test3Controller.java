2fdd819aad53bbd220e468083a051fcc5297ac49
杨顾 <yanggu@zetyun.com>
2023-03-23 17:56:03 +0800

    使用RingBuffer代替Java队列完整调试

--- metric-calculate-web/src/main/java/com/yanggu/metric_calculate/controller/Test3Controller.java
+++ metric-calculate-web/src/main/java/com/yanggu/metric_calculate/controller/Test3Controller.java
@@ -1,17 +1,26 @@
 package com.yanggu.metric_calculate.controller;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.thread.NamedThreadFactory;
-import com.lmax.disruptor.*;
-import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.EventTranslator;
+import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.Sequence;
+import com.lmax.disruptor.SequenceBarrier;
 import lombok.Data;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/test3")
@@ -43,6 +52,7 @@ public class Test3Controller {
     }
 
     @Data
+    @Slf4j
     public static final class MyRunnable implements Runnable {
 
         private RingBuffer<Event> ringBuffer;
@@ -50,6 +60,13 @@ public class Test3Controller {
         private SequenceBarrier sequenceBarrier;
 
         private Sequence sequence = new Sequence();
+
+        private List<Long> sequenceList = new ArrayList<>();
+
+        private final ScheduledThreadPoolExecutor scheduledThreadPoolExecutor =
+                new ScheduledThreadPoolExecutor(1, new NamedThreadFactory("攒批定时器线程", true));
+
+        private ScheduledFuture<?> scheduledFuture;
 
         @SneakyThrows
         @Override
@@ -59,13 +76,50 @@ public class Test3Controller {
             while (true) {
                 final long availableSequence = sequenceBarrier.waitFor(nextSequence);
                 while (nextSequence <= availableSequence) {
-                    Event event = ringBuffer.get(nextSequence);
+                    addEvent(nextSequence);
                     nextSequence++;
-                    System.out.println("消费数据" + event);
                 }
-                sequence.set(availableSequence);
             }
         }
+        
+        private synchronized void addEvent(Long nextSequence) {
+            sequenceList.add(nextSequence);
+            if (sequenceList.size() >= 4) {
+                flush(0L, nextSequence);
+            }
+            if (scheduledFuture == null) {
+                long registerTimeStamp = System.currentTimeMillis();
+                scheduledFuture = scheduledThreadPoolExecutor.schedule(() -> this.flush(registerTimeStamp, nextSequence), 5000, TimeUnit.MILLISECONDS);
+            }
+        }
+
+        public synchronized void flush(long registerTimeStamp, Long nextSequence) {
+            if (CollUtil.isEmpty(sequenceList)) {
+                return;
+            }
+            if (registerTimeStamp == 0L) {
+                log.info("攒批大小到, 当前时间: " + DateUtil.formatDateTime(new Date()) +
+                        ", list被消费了, 消费list大小" + sequenceList.size());
+            } else {
+                log.info("攒批时间到, 定时器注册时间: " + DateUtil.formatDateTime(new Date(registerTimeStamp)) +
+                        ", 当前时间: " + DateUtil.formatDateTime(new Date()) + ", list被消费了, 消费list大小" + sequenceList.size());
+            }
+            try {
+                System.out.println("-----------------");
+                sequenceList.stream()
+                        .map(temp -> ringBuffer.get(temp).getValue())
+                        .forEach(System.out::println);
+            } catch (Throwable e) {
+                log.error("攒批线程消费失败", e);
+            }
+            sequenceList.clear();
+            if (scheduledFuture != null) {
+                scheduledFuture.cancel(true);
+                scheduledFuture = null;
+            }
+            sequence.set(nextSequence);
+        }
+        
     }
 
     @Data
