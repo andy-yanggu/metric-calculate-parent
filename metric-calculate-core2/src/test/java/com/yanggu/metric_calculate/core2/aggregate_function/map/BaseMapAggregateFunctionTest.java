68cec24f293d59b56cc82fb6fdaf9ead0fce265c
杨顾 <yanggu@zetyun.com>
2023-03-27 16:28:13 +0800

    修复单元测试的错误

--- metric-calculate-core2/src/test/java/com/yanggu/metric_calculate/core2/aggregate_function/map/BaseMapAggregateFunctionTest.java
+++ metric-calculate-core2/src/test/java/com/yanggu/metric_calculate/core2/aggregate_function/map/BaseMapAggregateFunctionTest.java
@@ -1,11 +1,10 @@
 package com.yanggu.metric_calculate.core2.aggregate_function.map;
 
+import cn.hutool.core.lang.Pair;
 import com.yanggu.metric_calculate.core2.aggregate_function.numeric.SumAggregateFunction;
 import com.yanggu.metric_calculate.core2.field_process.multi_field_distinct.MultiFieldDistinctKey;
 import org.junit.Before;
 import org.junit.Test;
-import reactor.util.function.Tuple2;
-import reactor.util.function.Tuples;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -37,7 +36,7 @@ public class BaseMapAggregateFunctionTest {
         Map<MultiFieldDistinctKey, Double> accumulator = basemap.createAccumulator();
 
         MultiFieldDistinctKey multiFieldDistinctKey = new MultiFieldDistinctKey(Collections.singletonList("张三"));
-        Tuple2<MultiFieldDistinctKey, Double> tuple2 = Tuples.of(multiFieldDistinctKey, 100.0D);
+        Pair<MultiFieldDistinctKey, Double> tuple2 = Pair.of(multiFieldDistinctKey, 100.0D);
         Map<MultiFieldDistinctKey, Double> add = basemap.add(tuple2, accumulator);
         assertSame(add, accumulator);
         //应该是张三:100
@@ -49,7 +48,7 @@ public class BaseMapAggregateFunctionTest {
         assertEquals(200.0D, add.get(multiFieldDistinctKey), 0.0D);
 
         MultiFieldDistinctKey multiFieldDistinctKey2 = new MultiFieldDistinctKey(Collections.singletonList("李四"));
-        Tuple2<MultiFieldDistinctKey, Double> otherTuple2 = Tuples.of(multiFieldDistinctKey2, 100.0D);
+        Pair<MultiFieldDistinctKey, Double> otherTuple2 = Pair.of(multiFieldDistinctKey2, 100.0D);
         add = basemap.add(otherTuple2, accumulator);
         assertEquals(2, accumulator.size());
         //李四100
