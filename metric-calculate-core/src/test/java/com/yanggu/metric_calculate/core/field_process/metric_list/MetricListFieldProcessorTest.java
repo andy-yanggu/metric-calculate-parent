4a5fb79245c442abdbfb19277ad476e17b9af439
杨顾 <17621950294@163.com>
2025-02-19 09:41:45 +0800

    多表达式字段处理器单元测试类参数化改造

--- metric-calculate-core/src/test/java/com/yanggu/metric_calculate/core/field_process/metric_list/MetricListFieldProcessorTest.java
+++ metric-calculate-core/src/test/java/com/yanggu/metric_calculate/core/field_process/metric_list/MetricListFieldProcessorTest.java
@@ -1,8 +1,10 @@
 package com.yanggu.metric_calculate.core.field_process.metric_list;
 
 import com.yanggu.metric_calculate.core.pojo.aviator_express.AviatorExpressParam;
-import org.dromara.hutool.core.collection.ListUtil;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.HashMap;
 import java.util.List;
@@ -12,7 +14,7 @@ import static com.yanggu.metric_calculate.core.field_process.FieldProcessorTestB
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
+@DisplayName("多表达式字段处理器单元测试类")
 class MetricListFieldProcessorTest {
 
     @Test
@@ -36,29 +38,32 @@ class MetricListFieldProcessorTest {
         AviatorExpressParam aviatorExpressParam = new AviatorExpressParam();
         aviatorExpressParam.setExpress("test1");
 
-        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> getMetricListFieldProcessor(fieldMap, ListUtil.of(aviatorExpressParam), null));
+        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> getMetricListFieldProcessor(fieldMap, List.of(aviatorExpressParam), null));
         assertEquals("Aviator函数工厂类为空", runtimeException.getMessage());
     }
 
-    @Test
-    void process() throws Exception {
+    @ParameterizedTest
+    @DisplayName("验证process方法")
+    @CsvSource({"'aaa','bbb'", "'ccc','ddd'"})
+    void process(String param1, String param2) {
         Map<String, Class<?>> fieldMap = new HashMap<>();
         fieldMap.put("test1", String.class);
         fieldMap.put("test2", String.class);
+
         AviatorExpressParam aviatorExpressParam1 = new AviatorExpressParam();
         aviatorExpressParam1.setExpress("test1");
         AviatorExpressParam aviatorExpressParam2 = new AviatorExpressParam();
         aviatorExpressParam2.setExpress("test2");
-        MetricListFieldProcessor metricListFieldProcessor = getMetricListFieldProcessor(fieldMap, ListUtil.of(aviatorExpressParam1, aviatorExpressParam2));
+        MetricListFieldProcessor metricListFieldProcessor = getMetricListFieldProcessor(fieldMap, List.of(aviatorExpressParam1, aviatorExpressParam2));
 
         Map<String, Object> input = new HashMap<>();
-        input.put("test1", "aaa");
-        input.put("test2", "bbb");
+        input.put("test1", param1);
+        input.put("test2", param2);
 
         List<Object> process = metricListFieldProcessor.process(input);
         assertEquals(2, process.size());
-        assertEquals("aaa", process.get(0));
-        assertEquals("bbb", process.get(1));
+        assertEquals(param1, process.get(0));
+        assertEquals(param2, process.get(1));
     }
 
 }
