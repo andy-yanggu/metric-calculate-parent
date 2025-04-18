8f08c65ad973357b85a711345a89d8300ae05302
杨顾 <17621950294@163.com>
2025-04-18 11:27:12 +0800

    升级依赖版本

--- metric-calculate-function-test/src/test/java/com/yanggu/metric_calculate/test2/FuryDemo.java
+++ metric-calculate-function-test/src/test/java/com/yanggu/metric_calculate/test2/FuryDemo.java
@@ -1,11 +1,9 @@
 package com.yanggu.metric_calculate.test2;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
+import lombok.ToString;
 import org.apache.fury.Fury;
 import org.apache.fury.config.Language;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -21,37 +19,53 @@ public class FuryDemo {
         System.out.println(fury.deserialize(bytes));
     }
 
+    @ToString
     public static class SomeClass1 {
 
         Object f1;
+
         Map<Byte, Integer> f2;
 
     }
 
+    @ToString
     public static class SomeClass2 {
+
         Object f1;
+
         String f2;
+
         List<Object> f3;
+
         Map<Byte, Integer> f4;
+
         Byte f5;
+
         Short f6;
+
         Integer f7;
+
         Long f8;
+
         Float f9;
+
         Double f10;
+
         short[] f11;
+
         List<Short> f12;
+
     }
 
     public static Object createObject() {
         SomeClass1 obj1 = new SomeClass1();
         obj1.f1 = true;
-        obj1.f2 = ImmutableMap.of((byte) -1, 2);
+        obj1.f2 = Map.of((byte) -1, 2);
         SomeClass2 obj = new SomeClass2();
         obj.f1 = obj1;
         obj.f2 = "abc";
-        obj.f3 = Arrays.asList("abc", "abc");
-        obj.f4 = ImmutableMap.of((byte) 1, 2);
+        obj.f3 = List.of("abc", "abc");
+        obj.f4 = Map.of((byte) 1, 2);
         obj.f5 = Byte.MAX_VALUE;
         obj.f6 = Short.MAX_VALUE;
         obj.f7 = Integer.MAX_VALUE;
@@ -59,7 +73,7 @@ public class FuryDemo {
         obj.f9 = 1.0f / 2;
         obj.f10 = 1 / 3.0;
         obj.f11 = new short[]{(short) 1, (short) 2};
-        obj.f12 = ImmutableList.of((short) -1, (short) 4);
+        obj.f12 = List.of((short) -1, (short) 4);
         return obj;
     }
 
